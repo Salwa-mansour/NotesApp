440ce5b6d7a7675504dc7f69bcf783448ebd727c
Salwa-mansour <alqadasisal@gmail.com>
2024-01-29 17:51:36 +0300

    local storage working

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const createBtn = document.querySelector('#create-btn');
 let notes = document.querySelectorAll('.input-box');
 
 createBtn.addEventListener("click",generteNote);
+showNotes();
 function generteNote(){
     let inputBox =document.createElement("p");
     let deletBtnImg=document.createElement("img");
@@ -18,5 +19,29 @@ function generteNote(){
 notesContainer.addEventListener("click",function(e){
     if(e.target.className=="delete-btn"){
         e.target.parentElement.remove();
+        updateStorage();
+    }else if(e.target.className === "input-box"){
+      
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt=>{
+           
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
     }
 });
+//----------- Local Storage ------------
+//save all html as text becouase the notes html is generated from javascprt!?
+function updateStorage(){
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes")
+}
+document.addEventListener("keydown",event=>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
--- think.md
+++ think.md
@@ -1,1 +1,3 @@
 >change the icon image by svg using devs as source
+>learn about local storage 
+>can i update local storage befor refresh page  rather that each key click
